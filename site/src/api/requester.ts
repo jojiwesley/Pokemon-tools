718f8c0bc2e6b016d8ae65bbf0792715a8dfdd5b
jojiwesley <joji.wesley@hotmail.com>
2024-11-16 20:36:55 -0300

    refact: in requester.ts

--- site/src/api/requester.ts
+++ site/src/api/requester.ts
@@ -1,44 +1,46 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosResponse } from 'axios';
 
+// Tipagem genérica para a função get
 export const requester = (config: any, contentType?: string): any => {
-  const service = axios.create({
-    baseURL: config.baseURL,
-    ...config.options,
-  });
+   const service = axios.create({
+      baseURL: config.baseURL,
+      ...config.options,
+   });
 
-  service.interceptors.request.use(
-    (req) => {
-      req.headers = {
-        "Content-Type": contentType || "application/json",
-        "Access-Control-Allow-Origin": "*",
-        ...config,
-      };
+   service.interceptors.request.use(
+      (req) => {
+         req.headers = {
+            'Content-Type': contentType || 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            ...config,
+         };
 
-      return req;
-    },
-    (error) => Promise.reject(error)
-  );
+         return req;
+      },
+      (error) => Promise.reject(error)
+   );
 
-  return {
-    async get<T = any>(uri: string): Promise<AxiosResponse<T>> {
-      const response = await service.get<T>(uri);
-      return response;
-    },
-    async post<T = any>(uri: string, data: any): Promise<AxiosResponse<T>> {
-      const response = await service.post<T>(uri, data);
-      return response;
-    },
-    async put<T = any>(uri: string, data: any): Promise<AxiosResponse<T>> {
-      const response = await service.put<T>(uri, data);
-      return response;
-    },
-    async patch<T = any>(uri: string, data: any): Promise<AxiosResponse<T>> {
-      const response = await service.patch<T>(uri, data);
-      return response;
-    },
-    async delete<T = any>(uri: string, data: any): Promise<AxiosResponse<T>> {
-      const response = await service.delete<T>(uri, data);
-      return response;
-    },
-  };
+   return {
+      // Aqui, podemos declarar a tipagem genérica para que seja possível usar o tipo IPokemon
+      async get<T = any>(uri: string): Promise<AxiosResponse<T>> {
+         const response = await service.get<T>(uri);
+         return response;
+      },
+      async post<T = any>(uri: string, data: any): Promise<AxiosResponse<T>> {
+         const response = await service.post<T>(uri, data);
+         return response;
+      },
+      async put<T = any>(uri: string, data: any): Promise<AxiosResponse<T>> {
+         const response = await service.put<T>(uri, data);
+         return response;
+      },
+      async patch<T = any>(uri: string, data: any): Promise<AxiosResponse<T>> {
+         const response = await service.patch<T>(uri, data);
+         return response;
+      },
+      async delete<T = any>(uri: string, data: any): Promise<AxiosResponse<T>> {
+         const response = await service.delete<T>(uri, data);
+         return response;
+      },
+   };
 };
