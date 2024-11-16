1aafa8d7679226715219d54312ada4bcbf694340
jojiwesley <joji.wesley@hotmail.com>
2024-11-16 20:43:09 -0300

    refact: aspas

--- site/src/store/atoms/index.ts
+++ site/src/store/atoms/index.ts
@@ -1,26 +1,30 @@
-import { atom } from "recoil";
-import { IPokemon, IPokemonFetch } from "../../interface";
+import { atom } from 'recoil';
+import { IPokemon, IPokemonFetch } from '../../interface';
 
+// Define um estado global para armazenar o termo de busca do Pokémon.
 export const atomPokemonSearch = atom<string | undefined>({
-  key: "atomPokemonSearch",
-  default: undefined,
+   key: 'atomPokemonSearch',
+   default: undefined,
 });
 
+// Define um estado global para armazenar o resultado da busca de Pokémons.
 export const atomPokemonFetch = atom<IPokemonFetch[]>({
-  key: "atomPokemonFetch",
-  default: [],
+   key: 'atomPokemonFetch',
+   default: [],
 });
 
+// Este estado armazena um offset numérico, utilizado para paginação.
 export const atomPokemonOffset = atom<number>({
-  key: "atomPokemonOffset",
-  default: 0,
+   key: 'atomPokemonOffset',
+   default: 0,
 });
-
+// Armazena a lista completa de Pokémons do tipo IPokemon.
 export const atomPokemonList = atom<IPokemon[]>({
-  key: "atomPokemonList",
-  default: [],
+   key: 'atomPokemonList',
+   default: [],
 });
 
+// Armazena um único objeto de Pokémon do tipo IPokemon.
 export const atomPokemon = atom<IPokemon>({
    key: 'atomPokemon',
    default: undefined,
@@ -28,5 +32,5 @@ export const atomPokemon = atom<IPokemon>({
 
 export const atomSelectedPokemon = atom<IPokemon>({
    key: 'atomSelectedPokemon',
-  default: undefined,
+   default: undefined,
 });
